62648cfcabfbfa93c9f86c7fdd1133598b2de096
jianglin <jianglin@192.168.0.101>
2018-07-28 16:58:41 +0800

    chore: eslint autofix

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
 </template>
 
 <script>
-<<<<<<< 76da4c0a9dca675df2f7026d3dde2e7da1165c59
-  export default {}
-=======
-  export default {
-    mounted () {
-      console.log('ok');
-    }
-  };
->>>>>>> chore: 添加eslint
+  export default {};
 </script>
 
 <style>
